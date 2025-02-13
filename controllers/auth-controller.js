e7884143dc77fefd710b2aeb3dc7b73f8b02c647
tratchapong <tratchapong@gmail.com>
2025-02-13 16:25:55 +0700

    login ok

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken')
 const prisma = require('../models')
 const bcrypt = require('bcryptjs')
 
@@ -36,22 +37,22 @@ module.exports.register = async (req, res, next) => {
 		const identityKey = checkEmailorMobile(identity)
 
 		// หาว่ามี user นี้แล้วหรือยัง
-
 		const findIdentity = await prisma.user.findUnique({
 			where: { [identityKey]: identity }
 		})
-		if(findIdentity) {
+		if (findIdentity) {
 			createError(409, `Already have this user : ${identity}`)
 		}
-		
-		const newUser = {
-			[identityKey] : identity,
-			password : await bcrypt.hash(password, 10),
-			firstName : firstName,
-			lastName : lastName
-		}
 
-		const result = await prisma.user.create({data : newUser})
+		// เตรียมข้อมูล new user + hash password
+		const newUser = {
+			[identityKey]: identity,
+			password: await bcrypt.hash(password, 10),
+			firstName: firstName,
+			lastName: lastName
+		}
+		// สร้าง new user ใน database 
+		const result = await prisma.user.create({ data: newUser })
 		console.log(result)
 		res.json({ msg: `Register successful`, result })
 	} catch (err) {
@@ -60,8 +61,43 @@ module.exports.register = async (req, res, next) => {
 
 }
 
-module.exports.login = (req, res) => {
-	res.json({ msg: 'Login...' })
+module.exports.login = async (req, res, next) => {
+	try {
+		const { identity, password } = req.body
+		// validation
+		if (!identity.trim() || !password.trim()) {
+			createError(400, 'Please fill all data')
+		}
+	
+		// identity เป็น email หรือ mobile phone number
+		const identityKey = checkEmailorMobile(identity)
+	
+		// find user
+		const foundUser = await prisma.user.findUnique({
+			where: { [identityKey] : identity}
+		})
+	
+		if(!foundUser) {
+			createError(401, 'Invalid Login')
+		}
+	
+		// check password
+		let pwOk = await bcrypt.compare(password, foundUser.password)
+		if(!pwOk) {
+			createError(401, 'Invalid Login')
+		}
+	
+		// create jwt token
+	
+		const payload = { id: foundUser.id }
+		const token = jwt.sign(payload, process.env.JWT_SECRET, { 
+			expiresIn : '15d'
+		})
+		
+		res.json({ msg: 'Login successful', token: token, user: foundUser })
+	} catch(err) {
+		next(err)
+	}
 }
 
 module.exports.getMe = (req, res) => {
