d56e3cc7223f7a1d795a3a9f7a52e36de6e758cb
tratchapong <tratchapong@gmail.com>
2025-02-13 15:10:22 +0700

    Register : check email or mobile

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,6 +1,25 @@
 const createError = require("../utils/createError")
 
+function checkEmailorMobile(identity) {
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	const mobileRegex = /^[0-9]{10,15}$/
+
+	let identityKey = ''
+	if(emailRegex.test(identity)) {
+		identityKey = 'email'
+	}
+	if(mobileRegex.test(identity)) {
+		identityKey = 'mobile'
+	}
+	if(!identityKey) {
+		createError(400, 'only Email or Mobile phone')
+	}
+	return identityKey
+}
+
 module.exports.register = (req, res) => {
+
+
 	const {identity, firstName, lastName, password, confirmPassword} = req.body
 	// validation
 	if( !(identity.trim() && firstName.trim() && lastName.trim() && password.trim() && confirmPassword.trim()) ) {
@@ -10,9 +29,11 @@ module.exports.register = (req, res) => {
 	if( password !== confirmPassword) {
 		createError(400, 'please check confirm-password ')
 	}
-	console.log('After...validate')
 
-	res.json({msg : 'Register...'})
+	// identity เป็น email หรือ phone number
+	const identityKey = checkEmailorMobile(identity)
+	
+	res.json({msg : `Register with ${identityKey}`})
 }
 
 module.exports.login = (req, res) => {
