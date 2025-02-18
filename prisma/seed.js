cc9bd31a3d2b483b42818d194c9cf2c6b80787cb
tratchapong <tratchapong@gmail.com>
2025-02-18 17:06:32 +0700

    update seed.js : profileImage

--- prisma/seed.js
+++ prisma/seed.js
@@ -5,30 +5,23 @@ const hashedPassword = bcrypt.hashSync('123456', 10)
 
 const userData = [
 	{
-		firstName: 'Andy',
-		lastName : 'Codecamp',
-		email : 'andy@ggg.mail',
-		password : hashedPassword
+		firstName: 'Andy', lastName: 'Codecamp', password: hashedPassword, email: 'andy@ggg.mail',
+		profileImage: 'https://www.svgrepo.com/show/420364/avatar-male-man.svg'
 	},
 	{
-		firstName: 'Bobby',
-		lastName : 'Codecamp',
-		email : 'bobby@ggg.mail',
-		password : hashedPassword
+		firstName: 'Bobby', lastName: 'Codecamp', password: hashedPassword, email: 'bobby@ggg.mail',
+		profileImage: 'https://www.svgrepo.com/show/420319/actor-chaplin-comedy.svg'
 	},
 	{
-		firstName: 'Candy',
-		lastName : 'Codecamp',
-		mobile : '1111111111',
-		password : hashedPassword
+		firstName: 'Candy', lastName: 'Codecamp', password: hashedPassword, mobile: '1111111111',
+		profileImage: 'https://www.svgrepo.com/show/420327/avatar-child-girl.svg'
 	},
 	{
-		firstName: 'Danny',
-		lastName : 'Codecamp',
-		mobile : '2222222222',
-		password : hashedPassword
+		firstName: 'Danny', lastName: 'Codecamp', password: hashedPassword, mobile: '2222222222',
+		profileImage: 'https://www.svgrepo.com/show/420314/builder-helmet-worker.svg'
 	},
 ]
+
 
 console.log('DB seed...')
 
