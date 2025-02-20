52523309e298ae3c14f0b3909f124e34a10d61ac
tratchapong <tratchapong@gmail.com>
2025-02-20 13:37:54 +0700

    deletePost :ok

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -3,6 +3,7 @@ const fs = require('fs/promises')
 const tryCatch = require('../utils/tryCatch')
 const cloudinary = require('../config/cloudinary')
 const prisma = require('../models')
+const createError = require('../utils/createError')
 
 module.exports.createPost = tryCatch(async (req, res) => {
 	const {message} = req.body
@@ -36,7 +37,6 @@ module.exports.getAllPosts = tryCatch(async (req, res) => {
 				}
 			},
 			comments : {
-				// select : { message : true	},
 				include : {
 					user : {
 						select : {
@@ -62,6 +62,17 @@ module.exports.getAllPosts = tryCatch(async (req, res) => {
 module.exports.updatePost = async (req, res) => {
 	res.json({ msg: 'Update Post' })
 }
-module.exports.deletePost = async (req, res) => {
-	res.json({ msg: 'Delete Post' })
-}
+module.exports.deletePost = tryCatch(async (req, res) => {
+	const {id} = req.params
+
+	const postData = await prisma.post.findUnique({where : {id : +id} })
+	console.log(postData)
+	if(req.user.id !== postData.userId) {
+		createError(400,'Cannot delete')
+	}
+	const rs = await prisma.post.delete({
+		where : {id: +id}
+	})
+
+	res.json({ msg: `Delete post id=${id} done`, deletedPost : postData })
+})
