094c3f7e2c3130a5ce97fc484bd30d4432c00c50
Ratchapong Tantipantarak <39793075+tratchapong@users.noreply.github.com>
2025-06-13 16:46:37 +0700

    improve tryCatch.js to handle both sync + async function

--- utils/tryCatch.js
+++ utils/tryCatch.js
@@ -1,15 +1,32 @@
+// This function can handle both sync & async function
+
+module.exports = func => (req, res, next) => {
+	try {
+		const result = func(req, res, next);
+		if (result && typeof result.then === 'function') {
+			result.catch(next);
+		}
+	} catch (err) {
+		next(err);
+	}
+};
+
+
+
+// *** all below codes for async function only
+
 // module.exports = (func) => {
 // 	return (req, res, next) => func(req, res,next).catch(err => next(err))
 //  }
 
 // module.exports = func => (req,res,next) => func(req,res,next).catch(next)
 
-module.exports = func => {
-	return async function (req, res, next ) {
-		try {
-			await func(req, res, next)
-		}catch(err) {
-			next(err)
-		}
-	}
-}
+// module.exports = func => {
+// 	return async function (req, res, next ) {
+// 		try {
+// 			await func(req, res, next)
+// 		}catch(err) {
+// 			next(err)
+// 		}
+// 	}
+// }
